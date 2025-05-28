a42fe8c9fbc44f4251857150cc1ddcbcb7528bc2
Hudihi <Abdillahissa866@gmail.com>
2025-05-28 23:59:30 +0300

    add .env.example file for secrets details required in configuration and readMe file for instruction to running app

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from flask_cors import CORS
 import joblib
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-PORT=os.getenv('PORT')
-HOST=os.getenv('HOST')
+PORT = os.getenv('PORT')
+HOST = os.getenv('HOST')
 
 app = Flask(__name__)
-model=joblib.load('scam_sms_predict_model.pkl')
+model = joblib.load('scam_sms_predict_model.pkl')
 CORS(app)
+
 
 @app.route('/api/predict', methods=['POST'])
 def predict_scam_sms():
@@ -18,19 +20,20 @@ def predict_scam_sms():
         sms = request.json.get('sms')
     else:
         sms = request.form.get('sms')
-    
-    input_sms=sms
-    prediction=model.predict([input_sms])
-    if prediction==0:
+
+    input_sms = sms
+    prediction = model.predict([input_sms])
+    if prediction == 0:
         return {
             "prediction": "trust",
-            "sms":input_sms
-            }
+            "sms": input_sms
+        }
     else:
         return {
             "prediction": "scam",
-            "sms":input_sms
-            }
-    
+            "sms": input_sms
+        }
+
+
 if __name__ == '__main__':
-    app.run(host=HOST,port=PORT, debug=False)
+    app.run(host=HOST, port=PORT, debug=False)
